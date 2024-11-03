348d85d90c6e234e994842d41a89df201d670153
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-03 18:10:13 +0100

    Initial GitOnlineStore

---
+++ src/ua/ithillel/homework/lesson03/GitOnlineStore.java
@@ -0,0 +1,7 @@
+package ua.ithillel.homework.lesson03;
+
+public class GitOnlineStore {
+    public static void main(String[] args) {
+        System.out.println("This is shop-online app");
+    }
+}
