f18e26d64439853674a2ec0022a28fcf67b07d85
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-04 00:01:25 +0100

    Add lesson02 homework

---
+++ src/ua/ithillel/homework/lesson02/lesson03/lesson04/lesson02/OrdersOnlineStore.java
@@ -0,0 +1,18 @@
+package ua.ithillel.homework.lesson02.lesson03.lesson04.lesson02;
+
+public class OrdersOnlineStore {
+    public static void main(String[] args) {
+        int clientNumber = 1;
+        String clientName = "Alice";
+        String productName = "smartphone";
+        String address = "Moon Street, 10";
+        double productPrice = 305.99;
+        String CURRENCY = "EUR";
+        System.out.println("Order No " + clientNumber + " Client: " + clientName + ".");
+        System.out.println("Product: " + productName + ",");
+        System.out.println("Product: " + CURRENCY + " " + productPrice + ".");
+        System.out.println("Address: " + address);
+
+
+    }
+}
