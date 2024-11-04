27ad2efe9a7ca475db766406888f99a0f52add01
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-04 23:57:25 +0100

    Add variables for 1st product

--- src/ua/ithillel/homework/lesson02/lesson03/lesson04/SalesSum.java
+++ src/ua/ithillel/homework/lesson02/lesson03/lesson04/SalesSum.java
@@ -2,6 +2,9 @@ package ua.ithillel.homework.lesson02.lesson03.lesson04;
 
 public class SalesSum {
     public static void main(String[] args) {
-
+        int productNumber = 1;
+        String productName = "smartphone";
+        String CURRENCY = "EUR";
+        double salesByDay = 2430.68;
     }
 }
