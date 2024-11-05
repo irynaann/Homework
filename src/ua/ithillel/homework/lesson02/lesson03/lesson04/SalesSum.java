b0a7bdfe458159b6b188f27ae10b702b33c6ba68
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-05 22:11:05 +0100

    Change number salesByDay + output method

--- src/ua/ithillel/homework/lesson02/lesson03/lesson04/SalesSum.java
+++ src/ua/ithillel/homework/lesson02/lesson03/lesson04/SalesSum.java
@@ -15,8 +15,12 @@ public class SalesSum {
 
         productNumber = 2;
         productName = "laptop";
-        salesByDay = 10486.85F;
+        salesByDay = 1498.12F;
         totalDays = 7;
-
+        totalSales = totalDays * salesByDay;
+        AverageSalesByday = totalSales / totalDays;
+        System.out.printf("Product No %d: %s, %n", productNumber, productName);
+        System.out.printf("total sales for %d days is %s %.2f. %n", totalDays, CURRENCY, totalSales);
+        System.out.printf("sales by day is %s %.2f.",CURRENCY, AverageSalesByday);
     }
 }
