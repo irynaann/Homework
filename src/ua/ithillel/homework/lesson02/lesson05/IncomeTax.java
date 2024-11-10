1f37f144621e9c33c1a85222751d03342c3d7c3b
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-11 00:19:34 +0100

    Add if - tax calculation

--- src/ua/ithillel/homework/lesson02/lesson05/IncomeTax.java
+++ src/ua/ithillel/homework/lesson02/lesson05/IncomeTax.java
@@ -8,5 +8,18 @@ public class IncomeTax {
         System.out.println("Enter income: ");
         int income = scanner.nextInt();
         double taxRate;
+
+        if (income <= 10000) {
+            taxRate = income * 2.5 / 100;
+            System.out.println("Tax Amount: " + taxRate);
+        } else if (income < 10000 && income >= 25000) {
+            taxRate = income * 4.2 / 100;
+            System.out.println("Tax Amount: " + taxRate);
+        } else if (income < 25000) {
+            taxRate = income * 6.7 / 100;
+            System.out.println("Tax Amount: " + taxRate);
+        } else {
+            System.out.println("Undefined");
+        }
     }
 }
