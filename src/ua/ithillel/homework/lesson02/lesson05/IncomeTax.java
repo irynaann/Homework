c3f21fc10cfa663f6afa37625781187de90b65ad
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-11 16:01:07 +0100

    Rename taxRate

--- src/ua/ithillel/homework/lesson02/lesson05/IncomeTax.java
+++ src/ua/ithillel/homework/lesson02/lesson05/IncomeTax.java
@@ -7,17 +7,17 @@ public class IncomeTax {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter income: ");
         int income = scanner.nextInt();
-        double taxRate;
+        double tax;
 
         if (income <= 10000) {
-            taxRate = income * 2.5 / 100;
-            System.out.println("Tax Amount: " + taxRate);
+            tax = income * 2.5 / 100;
+            System.out.println("Tax Amount: " + tax);
         } else if (income > 10000 && income <= 25000) {
-            taxRate = income * 4.3 / 100;
-            System.out.println("Tax Amount: " + taxRate);
+            tax = income * 4.3 / 100;
+            System.out.println("Tax Amount: " + tax);
         } else if (income > 25000) {
-            taxRate = income * 6.7 / 100;
-            System.out.println("Tax Amount: " + taxRate);
+            tax = income * 6.7 / 100;
+            System.out.println("Tax Amount: " + tax);
         } else {
             System.out.println("Undefined");
         }
