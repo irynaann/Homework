16ceced2827c83b33dbd287f452f47a4c9d9ec51
irynaann <iryna.andriichenko@gmail.com>
2024-11-14 00:45:26 +0100

    Delete variable numeration and Integer

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
@@ -7,14 +7,11 @@ public class CycleFor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter maximum range of numbers: ");
-        int maxRange = Integer.MAX_VALUE;
         maxRange = scanner.nextInt();
 
         int sum = 0;
-        int numeration = 0;
         for (int num = 1; num <= maxRange; num++) {
             sum += num;
-            numeration++;
             System.out.println(numeration + ")" + " Num is " + num + " sum is: " + sum);
         }
         System.out.println("------------------------");
