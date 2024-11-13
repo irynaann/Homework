ba482cd23f47fcf261dd89902e9d3b6cb4bc754b
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-13 22:17:50 +0100

    Add cycle for

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class CycleFor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter maximum range of numbers: ");
+        int maxRange = Integer.MAX_VALUE;
+        maxRange = scanner.nextInt();
 
-
+        int sum = 0;
+        for (int num = 1; num <= maxRange; num++) {
+            sum += num;
+            System.out.println("Num is " + num + " sum is: " + sum);
+        }
     }
 }
