89a10542668d43285f7d926dfb37a50023cfacc6
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-13 21:35:26 +0100

    Add Scanner

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleFor.java
@@ -1,7 +1,11 @@
 package ua.ithillel.homework.lesson06.sumcalculator;
 
+import java.util.Scanner;
+
 public class CycleFor {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 }
