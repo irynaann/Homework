0f0fca05ac0453eed81e55dbda288f3cc9b8b048
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-13 22:35:57 +0100

    Add Scanner

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleWhile.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleWhile.java
@@ -1,7 +1,14 @@
 package ua.ithillel.homework.lesson06.sumcalculator;
 
+import java.util.Scanner;
+
 public class CycleWhile {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter maximum range of numbers: ");
+        int maxRange = Integer.MAX_VALUE;
+        maxRange = scanner.nextInt();
 
     }
 }
+
