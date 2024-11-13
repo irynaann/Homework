bf0a7036d6c628bdb909caec3c8c3e056e0cdc67
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-14 00:33:38 +0100

    Delete Integer.MAX_VALUE

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleDoWhile.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleDoWhile.java
@@ -6,8 +6,7 @@ public class CycleDoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter maximum range of numbers: ");
-        int maxRange = Integer.MAX_VALUE;
-        maxRange = scanner.nextInt();
+        int maxRange = scanner.nextInt();
 
         int num = 1;
         int sum = 0;
