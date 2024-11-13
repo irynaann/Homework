1664053b53ded5657e7db81b8f07932f3bf2d449
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-13 23:07:53 +0100

    Add cycle do - while

--- src/ua/ithillel/homework/lesson06/sumcalculator/CycleDoWhile.java
+++ src/ua/ithillel/homework/lesson06/sumcalculator/CycleDoWhile.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 public class CycleDoWhile {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter maximum range of numbers: ");
         int maxRange = Integer.MAX_VALUE;
         maxRange = scanner.nextInt();
 
+        int num = 1;
+        int sum = 0;
+        int numeration = 1;
+        do {
+            System.out.println(numeration + ")" + " Num is "
+                    + num + "," + " sum is: " + sum);
+            numeration++;
+            sum += num;
+            num++;
+        } while (num <= maxRange);
     }
 }
