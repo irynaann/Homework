b8e383912bba123ba8915a6a428a8993bd6b0450
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-24 23:45:01 +0100

    Add sorted array

--- src/ua/ithillel/homework/lesson08/ArraysAlgorithm.java
+++ src/ua/ithillel/homework/lesson08/ArraysAlgorithm.java
@@ -12,8 +12,22 @@ public class ArraysAlgorithm {
         int[] array = new int[value];
         System.out.print("Initial array: ");
         for (int i = 0; i < array.length; i++) {
-            array[i] = ThreadLocalRandom.current().nextInt(1,101);
+            array[i] = ThreadLocalRandom.current().nextInt(1, 101);
             System.out.print(array[i] + " ");
+        }
+        for (int i = 1; i < array.length; i++) {
+            int currentIndex = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > currentIndex) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j+1] = currentIndex;
+        }
+        System.out.println();
+        System.out.print("Sorted array: ");
+        for(int arr: array) {
+            System.out.print(arr + " ");
         }
     }
 }
