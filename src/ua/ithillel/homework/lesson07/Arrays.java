ea24c6dc4718968c9ef105230366fcafd40ac640
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-24 17:17:47 +0100

    Fix sum of negative numbers

--- src/ua/ithillel/homework/lesson07/Arrays.java
+++ src/ua/ithillel/homework/lesson07/Arrays.java
@@ -10,44 +10,54 @@ public class Arrays {
         int value = scanner.nextInt();
 
         int[] num = new int[value];
+        System.out.print("Елементи масиву: ");
         for (int i = 0; i < num.length; i++) {
             num[i] = ThreadLocalRandom.current().nextInt(-100, 101);
-            System.out.println(num[i]);
+            System.out.print(num[i] + " ");
         }
+        System.out.println();
+
         int sum = 0;
         for (int i = 0; i < num.length; i++) {
             if (num[i] < 0) {
                 sum += num[i];
-                System.out.println("Cума від'ємних чисел: " + sum);
             }
         }
+        System.out.print("Cума від'ємних чисел: " + sum);
+        System.out.println();
+        System.out.print("Кількість парних чисел: ");
         int count = 0;
         for (int i = 0; i < num.length; i++) {
             if (num[i] % 2 == 0) {
                 count += num.length;
-                System.out.println("Кількість парних чисел: " + count);
+                System.out.print(count + " ");
             }
         }
+        System.out.println();
+        System.out.print("Кількість непарних чисел: ");
         for (int i = 0; i < num.length; i++) {
             if (num[i] % 2 != 0) {
                 count += num.length;
-                System.out.println("Кількість непарних чисел: " + count);
+                System.out.print(count + " ");
             }
         }
+        System.out.println();
         int min = 0;
         int max = 0;
         for (int i = 0; i < num.length; i++) {
             if (num[i] < min) {
                 min = num[i];
-                System.out.println("Найменший елемент: " + min + " з індексом " + i);
+                System.out.print("Найменший елемент: " + min + " з індексом " + i);
             }
         }
+        System.out.println();
         for (int i = 0; i < num.length; i++) {
             if (num[i] > max) {
                 max = num[i];
-                System.out.println("Найбільший елемент: " + max + " з індексом " + i);
+                System.out.print("Найбільший елемент: " + max + " з індексом " + i);
             }
         }
+        System.out.print("Середнє арифметичне чисел після першого від'ємного числа: ");
         int sum2 = 0;
         int average;
         for (int i = 0; i < num.length; i++) {
@@ -55,11 +65,8 @@ public class Arrays {
                 min = num[i];
                 sum2 += min;
                 average = sum2 / num.length;
-                System.out.println("Середнє арифметичне чисел після першого від'ємного числа: " + average);
-            } else {
-                System.out.println("There are no negative numbers");
-            }
-
+                System.out.println(average);
+            } System.out.println("There are no negative numbers");
         }
     }
 }
