053c9ecd7706f62c531a0ae027d8c74187bcead4
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-24 15:46:31 +0100

    Add sum negative numbers

--- src/ua/ithillel/homework/lesson07/Arrays.java
+++ src/ua/ithillel/homework/lesson07/Arrays.java
@@ -1,15 +1,26 @@
 package ua.ithillel.homework.lesson07;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] num = new int[20];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add value: ");
+        int value = scanner.nextInt();
+
+        int[] num = new int[value];
         for (int i = 0; i < num.length; i++) {
             num[i] = ThreadLocalRandom.current().nextInt(-100, 101);
-
+            System.out.println(num[i]);
         }
-
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+        if(num[i] < 0) {
+            sum += num[i];
+            System.out.println("Cума: " + sum);
+        }
+        }
 
     }
 }
