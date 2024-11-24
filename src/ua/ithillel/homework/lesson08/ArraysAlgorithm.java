4afe2819a8278f6bf6b7511e529476be41c5fd46
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-25 00:39:54 +0100

    Add binary search algorithm

--- src/ua/ithillel/homework/lesson08/ArraysAlgorithm.java
+++ src/ua/ithillel/homework/lesson08/ArraysAlgorithm.java
@@ -22,12 +22,44 @@ public class ArraysAlgorithm {
                 array[j + 1] = array[j];
                 j--;
             }
-            array[j+1] = currentIndex;
+            array[j + 1] = currentIndex;
         }
         System.out.println();
         System.out.print("Sorted array: ");
-        for(int arr: array) {
+        for (int arr : array) {
             System.out.print(arr + " ");
         }
+        System.out.println();
+
+        System.out.println("Number to search: ");
+        int target = scanner.nextInt();
+        int result = binarySearch(array, target);
+        if (result == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+
+        }
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+
+            }
+        }
+        return -1;
     }
 }
+
+
+
+
