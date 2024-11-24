95c4e047a000a4f3fe8df24bdb1f0e984263e69c
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-24 14:52:10 +0100

    Add random num

--- src/ua/ithillel/homework/lesson07/Arrays.java
+++ src/ua/ithillel/homework/lesson07/Arrays.java
@@ -1,8 +1,15 @@
 package ua.ithillel.homework.lesson07;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Arrays {
     public static void main(String[] args) {
         int[] num = new int[20];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = ThreadLocalRandom.current().nextInt(-100, 101);
+
+        }
+
 
     }
 }
