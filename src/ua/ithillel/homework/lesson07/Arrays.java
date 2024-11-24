5acd18abbafe438aa8998681be78d2469072ee54
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-11-24 21:00:07 +0100

    Add average

--- src/ua/ithillel/homework/lesson07/Arrays.java
+++ src/ua/ithillel/homework/lesson07/Arrays.java
@@ -7,8 +7,8 @@ public class Arrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Add value: ");
-        int value = scanner.nextInt();
-
+        // int value = scanner.nextInt();
+        int value = 4;
         int[] num = new int[value];
         System.out.print("Елементи масиву: ");
         for (int i = 0; i < num.length; i++) {
@@ -41,8 +41,8 @@ public class Arrays {
         }
         System.out.println("Кількість непарних чисел: " + oddCount);
 
-        int min= 0;
-        int minIndex= 0;
+        int min = 0;
+        int minIndex = 0;
         for (int i = 0; i < num.length; i++) {
             if (num[i] < minIndex) {
                 min = num[i];
@@ -52,27 +52,40 @@ public class Arrays {
         System.out.println("Найменший елемент: " + min + " з індексом " + minIndex);
 
         int max = 0;
-        int maxIndex= 0;
+        int maxIndex = 0;
         for (int i = 0; i < num.length; i++) {
             if (num[i] > max) {
                 max = num[i];
                 maxIndex = i;
             }
         }
-        System.out.print("Найбільший елемент: " + max + " з індексом " + maxIndex);
+        System.out.println("Найбільший елемент: " + max + " з індексом " + maxIndex);
 
-        System.out.print("Середнє арифметичне чисел після першого від'ємного числа: ");
-        int sum2 = 0;
-        int average;
-//        for (int i = 0; i < num.length; i++) {
-//            if (num[i] < min) {
-//                min = num[i];
-//                sum2 += min;
-//                average = sum2 / num.length;
-//                System.out.println(average);
-//            }
-//            System.out.println("There are no negative numbers");
+        int index = 0;
+        while (index < num.length && num [index] >= 0) {
+            index++;
         }
+        System.out.println("index == " + index);
+        if (index >= num.length){
+            System.out.println("No negative elements");
+        }else {
+            sum = 0;
+            for (int i = index + 1; i < num.length; i++) {
+                sum += num[i];
+            }
+            System.out.println("Sum of elements: " + sum);
+            if (index == num.length - 1) {
+                System.out.println("No elements");
+            } else {
+                double average = (double) sum / (num.length - index - 1);
+                System.out.println("Average == " + average);
+
+
+            }
+        }
+
     }
+}
+
 
 
