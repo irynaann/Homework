0faac351baf7d1c70c8c7bf9c7020d07166f1d46
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-08 22:56:51 +0100

    Add methods

--- src/ua/ithillel/homework/lesson09/MultidimensionalArrays.java
+++ src/ua/ithillel/homework/lesson09/MultidimensionalArrays.java
@@ -16,54 +16,64 @@ public class MultidimensionalArrays {
 
         int[][] matrix = new int[size][range];
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = ThreadLocalRandom.current().nextInt(1, 5);
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("Sum of elements in even rows: ");
+        System.out.println("Sum of elements in even rows: " + calculateEvenSum(matrix));
+        System.out.println("Sum of elements in odd rows: " + calculateOddSum(matrix));
+        System.out.println("Product of elements in even columns: " + calculateEvenProduct(matrix));
+        System.out.println("Product of elements in odd columns: " + calculateOddProduct(matrix));
+    }
+
+    public static int calculateEvenSum(int[][] matrix) {
         int evenSum = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (i % 2 == 0) {
                     evenSum += matrix[i][j];
                 }
             }
         }
-        System.out.println(evenSum);
-        System.out.println("Sum of elements in odd rows: ");
+        return evenSum;
+    }
+
+    public static int calculateOddSum(int[][] matrix) {
         int oddSum = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (i % 2 != 0) {
                     oddSum += matrix[i][j];
                 }
             }
         }
-        System.out.println(oddSum);
-        System.out.println("Product of elements in even columns: ");
+        return oddSum;
+    }
+
+    public static int calculateEvenProduct(int[][] matrix) {
         int evenProduct = 1;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (j % 2 == 0) {
                     evenProduct *= matrix[i][j];
                 }
             }
         }
-        System.out.println(evenProduct);
-        System.out.println("Product of elements in odd columns: ");
+        return evenProduct;
+    }
+
+    public static int calculateOddProduct(int[][] matrix) {
         int oddProduct = 1;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (j % 2 != 0) {
                     oddProduct *= matrix[i][j];
                 }
             }
         }
-        System.out.println(oddProduct);
-
-
+        return oddProduct;
     }
 }
 
