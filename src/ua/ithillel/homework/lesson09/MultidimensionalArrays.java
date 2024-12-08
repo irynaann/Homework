19586605c780c39ff3c7f52e9ba08e373accd9f4
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-08 20:56:38 +0100

    Add table matrix

--- src/ua/ithillel/homework/lesson09/MultidimensionalArrays.java
+++ src/ua/ithillel/homework/lesson09/MultidimensionalArrays.java
@@ -1,15 +1,27 @@
 package ua.ithillel.homework.lesson09;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MultidimensionalArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter size: ");
-        int size = scanner.nextInt();
+        //int size = scanner.nextInt();
+        int size = 4;
         System.out.println("Enter range: ");
-        int range = scanner.nextInt();
+        //int range = scanner.nextInt();
+        int range = 4;
+        System.out.println("Матриця: " );
 
-
+        int[][] matrix = new int[size][range];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = ThreadLocalRandom.current().nextInt(1,51);
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-}
+    }
+
