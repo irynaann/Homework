c861bbca00aea585ab91ffeb6c0e84ef9e71b408
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-08 20:40:55 +0100

    Add Scanner

---
+++ src/ua/ithillel/homework/lesson09/MultidimensionalArrays.java
@@ -0,0 +1,15 @@
+package ua.ithillel.homework.lesson09;
+
+import java.util.Scanner;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size: ");
+        int size = scanner.nextInt();
+        System.out.println("Enter range: ");
+        int range = scanner.nextInt();
+
+
+    }
+}
