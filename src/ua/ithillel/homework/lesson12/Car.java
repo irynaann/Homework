0d02eafc2b4bcc368dd0fd1db3b7737fb6944f0c
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-15 20:38:05 +0100

    Add methods for Car

--- src/ua/ithillel/homework/lesson12/Car.java
+++ src/ua/ithillel/homework/lesson12/Car.java
@@ -1,4 +1,36 @@
 package ua.ithillel.homework.lesson12;
 
 public class Car {
+    public Car() {
+    }
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    public void stop() {
+        stopElectricity();
+        stopCommand();
+        stopFuelSystem();
+    }
+
+    private void startElectricity() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startFuelSystem() {
+    }
+
+    private void stopElectricity() {
+    }
+
+    private void stopCommand() {
+    }
+
+    private void stopFuelSystem() {
+    }
 }
--- src/ua/ithillel/homework/lesson12/DemoCar.java
+++ src/ua/ithillel/homework/lesson12/DemoCar.java
@@ -1,4 +1,8 @@
 package ua.ithillel.homework.lesson12;
 
 public class DemoCar {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+    }
 }
