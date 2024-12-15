7eca3d2400ab15bb7462ee16b07b01769dedf138
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-15 20:58:45 +0100

    Add DemoClass

--- src/ua/ithillel/homework/lesson12/DemoClass.java
+++ src/ua/ithillel/homework/lesson12/DemoClass.java
@@ -3,7 +3,7 @@ package ua.ithillel.homework.lesson12;
 public class DemoClass {
     public static void main(String[] args) {
         Car car = new Car();
-        Employee employee = new Employee(30,56664554,"bank@gmail.com","analytic","Barbara Braun");
+        Employee employee = new Employee(30, 56664554, "bank@gmail.com", "analytic", "Barbara Braun");
         System.out.println("----Full Name----");
         System.out.println(employee.getFullName());
         System.out.println("----Phone Number----");
