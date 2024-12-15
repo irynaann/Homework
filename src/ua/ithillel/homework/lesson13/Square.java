fd8fd495f40bd0259a84bddf49a08d75b122a771
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-15 23:15:40 +0100

    Override areaShape Square

--- src/ua/ithillel/homework/lesson13/Square.java
+++ src/ua/ithillel/homework/lesson13/Square.java
@@ -1,4 +1,14 @@
 package ua.ithillel.homework.lesson13;
 
-public class Square implements GeometricShape{
+public class Square implements GeometricShape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double areaShape() {
+        return side * side;
+    }
 }
