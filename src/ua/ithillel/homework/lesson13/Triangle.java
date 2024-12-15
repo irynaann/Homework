c7950ec6ec925bce5508a931331f4ef0c41c754c
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-15 23:10:58 +0100

    Override areaShape Triangle

--- src/ua/ithillel/homework/lesson13/Triangle.java
+++ src/ua/ithillel/homework/lesson13/Triangle.java
@@ -1,4 +1,16 @@
 package ua.ithillel.homework.lesson13;
 
 public class Triangle implements GeometricShape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double areaShape() {
+        return 0.5 * base * height;
+    }
 }
