6cd49f22225f59ab1cd114d37c578f21876f94d6
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-23 00:46:37 +0100

    Add day advise method

---
+++ src/ua/ithillel/homework/lesson15/Advise.java
@@ -0,0 +1,5 @@
+package ua.ithillel.homework.lesson15;
+
+public interface Advise {
+    void advise(Day day);
+}
--- src/ua/ithillel/homework/lesson15/DayAdvice.java
+++
@@ -1,5 +0,0 @@
-package ua.ithillel.homework.lesson15;
-
-public class DayAdvice {
-
-}
